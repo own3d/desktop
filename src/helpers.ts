308bc0a5f37bc663367ec3505c20bf5345cc28ef
René Preuß <rene@preuss.io>
2024-06-18 18:02:19 +0200

    Bump version Add isDestroyed check

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,9 @@
-import {BrowserWindow, WebviewTag} from "electron";
+import { BrowserWindow } from 'electron'
 
 export function emit(event: any, ...args: any) {
     // Send a message to all windows
     BrowserWindow.getAllWindows().forEach((win) => {
+        if (win.isDestroyed()) return
         win.webContents.send(event, ...args)
-    });
+    })
 }
